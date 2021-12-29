4fbb47823c5b6a953fb9b914d0feeaa0f5531e24
Jhonatan Becerra <jbecerrap95@gmail.com>
2021-12-29 11:00:57 -0500

    day#4

---
+++ day#4.js
@@ -0,0 +1,57 @@
+// ¡Es hora de poner el árbol de navidad en casa! 🎄
+
+// Para ello vamos a crear una función que recibe la altura del árbol, que será un entero positivo del 1 a, como máximo, 100.
+
+// Si le pasamos el argumento 5, se pintaría esto:
+// ____*____
+// ___***___
+// __*****__
+// _*******_
+// *********
+// ____#____
+// ____#____
+
+// Creamos un triángulo de asteriscos * con la altura proporcionada y, a los lados, usamos el guión bajo _ para los espacios. Es muy importante que nuestro árbol siempre tenga la misma longitud por cada lado.
+// Todos los árboles, por pequeños o grandes que sean, tienen un tronco de dos líneas de #.
+
+// Otro ejemplo con un árbol de altura 3:
+// __*__
+// _***_
+// *****
+// __#__
+// __#__
+
+// Ten en cuenta que el árbol es un string y necesitas los saltos de línea \n para cada línea para que se forme bien el árbol.
+
+function createXmasTree(height) {
+  
+    let star = ''
+    let trunk = ''
+  
+    for(let i = 0; i < height; i++){
+       for(let space = 1; space < height - i; space++){
+         if(i === 0)trunk += '_'
+         star += '_'
+       }
+        for(let tree = 0; tree <= i; tree++){
+          if(i === 0)trunk += '#'
+          if(tree == 0){
+            star += '*'
+          }else{
+          star += '**'
+          }
+        }
+        for(let space = 1; space < height - i; space++){
+          if(i === 0)trunk += '_'
+         star += '_'
+       }
+  
+       star += '\n'
+    }
+    star += trunk + '\n'
+    star += trunk
+  
+  
+    return star
+    return ''
+  }
